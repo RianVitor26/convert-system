64aee9d12ded3fc9059627570405b92830f50020
RianVitor26 <rianvitorlhe@gmail.com>
2022-04-13 12:39:41 -0300

    Fazendo a lógica do menu

---
+++ main.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.btn')
+const panels = document.querySelectorAll('.panel')
+
+toggle()
+
+function toggle() {
+    buttons.forEach(btn => btn.addEventListener("click", () => add(event.target)))
+}
+
+function add(btn) {
+    resetAll()
+    let dataset = document.querySelectorAll(`[data-js="${btn.dataset.js}"]`)
+    dataset.forEach(dataset => dataset.classList.add('active'))
+}
+
+function resetAll() {
+    buttons.forEach(btn => btn.classList.remove('active'))
+    panels.forEach(panel => panel.classList.remove('active'))
+}
